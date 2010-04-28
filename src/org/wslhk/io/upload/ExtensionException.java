ee4bc8ccc070c741399de6db194b6d926c5f1895
siliang wei <wslhk@163.com>
2010-04-28 23:20:16 +0800

    home

--- src/org/wslhk/io/upload/ExtensionException.java
+++ src/org/wslhk/io/upload/ExtensionException.java
@@ -1,12 +1,15 @@
 package org.wslhk.io.upload;
 
 public class ExtensionException extends Exception{
-	String message="Extension of file is error";
+	String message="Extension of file is error/文件格式不支持";
 	public ExtensionException(){	
 		//this.getMessage()
 		
 	}
 	public String getMessage(){
+		return message;
+	}
+	public String toString(){
 		return message;
 	}
 }
--- src/org/wslhk/io/upload/Upload.java
+++ src/org/wslhk/io/upload/Upload.java
@@ -7,35 +7,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.ProgressListener;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import org.wslhk.str.Str;
-
 
 public class Upload {
 void upload(){
 }
-List fieldlist=new ArrayList();
-List filelist=new ArrayList();
-
+//表单对象
+List<Field> fieldlist=new ArrayList();
+//文件对象
+List<Files> filelist=new ArrayList();
+//真实文件名
 public static int TRUENAME=0;
+//随机文件名
 public static int RNDNAME=1;
+//设置文件名
 public static int SETNAME=2;
-
+//最大尺寸限制
 long maxsize=0;
+//文件名
 String name=null;
+//文件保存路径
 String path=null;
+//临时路径
 String temppath=null;
+//后缀名
 String extension=null;
+//当前类型
 int nametype=0;
 
-AjaxCurrent ajax=new AjaxCurrent();
-HttpServletRequest request=null;
-
-/*默认上传*/
+UploadProgress progressListener = new UploadProgress();  
+/**
+ * 默认上传
+ * @param request
+ * @throws Exception
+ */
 public void upload(HttpServletRequest request)throws Exception{
-	this.request=request;
+//	Thread thread=new Thread(
+//			 new Runnable() { public void run() { 
+//			 int i=0;
+//				 while(i<5000);
+//					 try{
+//					 	Thread.sleep(1);
+//					 }catch(Exception e){
+//					 	e.printStackTrace();
+//					 }
+//				 i++;
+//				 ProgressListener pl= (ProgressListener)request.getSession().getAttribute("progress");
+//				 }
+//			 
+//			 }
+//			 );
+	
 if(temppath==null){temppath=path;}
 	   File tempPathFile=new File(temppath);
 	   if(tempPathFile.exists()==false){tempPathFile.mkdirs();}
@@ -50,11 +74,16 @@ if(temppath==null){temppath=path;}
 
     // Create a new file upload handler
     ServletFileUpload upload = new ServletFileUpload(factory);
+    
 
     upload.setSizeMax(maxsize); // 设置最大文件尺寸，这里是200k
 try{
     List<FileItem> items = upload.parseRequest(request);// 得到所有的文件
-   
+    //添加监听
+    //uploadsetProgressListener(new UploadListener(request));
+    request.getSession().setAttribute("progress", progressListener);   
+    upload.setProgressListener(progressListener);   
+    
     Iterator<FileItem> i = items.iterator();
     while (i.hasNext()) {
        FileItem fi = (FileItem) i.next();
@@ -70,6 +99,10 @@ try{
 }
 	
 }
+/**
+ * 添加表单内容
+ * @param fi
+ */
 private void formadd(FileItem fi){
 	Field f=new Field();
 	f.setName(fi.getFieldName());
@@ -77,6 +110,11 @@ private void formadd(FileItem fi){
 	//System.out.println(f.getName()+"___"+f.getValue());
 	fieldlist.add(f);
 }
+/**
+ * 通过提交名称查找一个文本域对象
+ * @param fieldname
+ * @return
+ */
 public Field getField(String fieldname){
 	Field f=new Field();
 	for(int i=0;i<this.fieldlist.size();i++){
@@ -86,44 +124,78 @@ public Field getField(String fieldname){
 		}
 	}
 	f=null;
-	return new Field();
+	return f;
 }
+
+/**
+ * 通过提交名称查找一个文件列表
+ * @param fieldname
+ * @return
+ */
+public List<Files> getFilelist(String fieldname){
+	Files f=new Files();
+	List<Files> listfileresult=new ArrayList();
+	for(int i=0;i<this.filelist.size();i++){
+		f=(Files)this.filelist.get(i);
+		if(f.getFieldname().equals(fieldname)){
+			listfileresult.add(f);
+		}
+		
+	}
+	return listfileresult;
+}
+
+/**
+ * 文件处理
+ * @param fi
+ * @throws ExtensionException
+ * @throws Exception
+ */
 private void fileadd(FileItem fi)throws ExtensionException,Exception{
-	//fi.g
+	//随机对象
 	java.util.Random r=new java.util.Random();
+	//日历对象
 	Calendar ca=Calendar.getInstance();
+	//文件对象
 	Files thisfile=new Files();
+	//查看是否支持后缀名
 	if(extension!=null&&this.isAllowExtension(fi.getName(), extension)==false){
    	  throw new ExtensionException();
      }
+	//设置后缀名
 	thisfile.setExt(getExtension(fi.getName()));
+	//设置路径
 	thisfile.setPath(this.getPath());
+	//设置文件大小
 	thisfile.setSize(fi.getSize());
+	//设置真实文件名
 	thisfile.setFilename(getFilename(fi.getName()));
+	//设置域名
 	thisfile.setFieldname(fi.getFieldName());
-	
-	setAjaxfilename(getFilename(fi.getName())+""+getExtension(fi.getName()));
-	
+	//如果设置的是随机文件名,重新设置保存随机文件名
 	if(this.getNametype()==this.RNDNAME){
 		thisfile.setFilename(""+ca.get(ca.YEAR)+ca.get(ca.MONTH)+ca.get(ca.DAY_OF_MONTH)+ca.get(ca.HOUR_OF_DAY)+ca.get(ca.MINUTE)+ca.get(ca.SECOND)+"_"+r.nextInt(999));
 	}else if(this.getNametype()==this.TRUENAME){
 		//donothing
-	}
-	if(this.getNametype()==this.SETNAME){
+	}else if(this.getNametype()==this.SETNAME){
+		//如果设置自定义文件名
+		
+		//字段名必须包含file
 		if(thisfile.getFieldname().indexOf("file")==-1){
 			throw new FileNameException("field_name,can't indexof 'file'");
 		}
 		if(thisfile.getFieldname().split("file")[0].length()==1){
 			throw new FileNameException("field_name,only indexof 'file'");
 		}
-			//this
+			//获得 file后的编号
 			String thisid=thisfile.getFieldname().split("file")[1];
-			
+			//设置的文件名必须在上传文件前设置,必须以filename开头,编号必须和文件相同
 			String t_setfname=null;
 			for(int i=0;i<this.getFieldlist().size();i++){
 				//t_setfname=((Field)this.getFieldlist().get(i)).getName();
 				if(((Field)this.getFieldlist().get(i)).getName().equals("filename"+thisid)){
 					t_setfname=((Field)this.getFieldlist().get(i)).getName();
+					break;
 				}
 			}
 			if(t_setfname==null){
@@ -131,9 +203,12 @@ private void fileadd(FileItem fi)throws ExtensionException,Exception{
 			}
 			thisfile.setFilename(t_setfname);
 	}
+	//保存文件组合
     File savedFile = new File(thisfile.getPath(),thisfile.getFilename()+"."+thisfile.getExt());
-    setAjaxfilename(thisfile.getFilename()+"."+thisfile.getExt());
+    
+    
     fi.write(savedFile);
+    System.out.println("upload.java:"+progressListener.getAllSize());
     filelist.add(thisfile);
     fi.delete();
 }
@@ -163,6 +238,12 @@ public String getFilename(String filenamefull){
 	 // }
 	 // return temp[leng-1];
 }
+/**
+ * 是否允许后缀名
+ * @param filename
+ * @param Extension
+ * @return
+ */
 public boolean isAllowExtension(String filename,String Extension){
 	/**
 	 * Extension用|分割
@@ -170,10 +251,9 @@ public boolean isAllowExtension(String filename,String Extension){
 	boolean result=false;
 	String fileext=getExtension(filename.toLowerCase());
 	String[] temp=Extension.toLowerCase().split("[|]");
-	for(int i=0;i<temp.length-1;i++){
-		//System.out.println(temp[i]+"=?"+fileext);
+	for(int i=0;i<temp.length;i++){
+	
 		if(temp[i].equals(fileext)){
-			
 			result=true;
 			break;
 		}
@@ -210,16 +290,16 @@ public int getNametype() {
 public void setNametype(int nametype) {
 	this.nametype = nametype;
 }
-public List getFieldlist() {
+public List<Field> getFieldlist() {
 	return fieldlist;
 }
-public void setFieldlist(List fieldlist) {
+public void setFieldlist(List<Field> fieldlist) {
 	this.fieldlist = fieldlist;
 }
-public List getFilelist() {
+public List<Files> getFilelist() {
 	return filelist;
 }
-public void setFilelist(List filelist) {
+public void setFilelist(List<Files> filelist) {
 	this.filelist = filelist;
 }
 public String getTemppath() {
@@ -228,28 +308,9 @@ public String getTemppath() {
 public void setTemppath(String temppath) {
 	this.temppath = temppath;
 }
-
-private void setAjaxfilename(String filename){
-	if(ajax==null){ajax=new AjaxCurrent();}
-	ajax.setFilename(filename);
-	request.getSession().setAttribute("ajaxupload", ajax);
+public ProgressListener getProgressListener() {
+	return progressListener;
 }
 
-private void setAjaxsize(long size){
-	if(ajax==null){ajax=new AjaxCurrent();}
-	ajax.setSize(size);
-	request.getSession().setAttribute("ajaxupload", ajax);
-}
-
-private void setAjaxCurrentsize(long size){
-	if(ajax==null){ajax=new AjaxCurrent();}
-	ajax.setCurrentsize(size);
-	request.getSession().setAttribute("ajaxupload", ajax);
-}
-private void setAjaxMs(long ms){
-	if(ajax==null){ajax=new AjaxCurrent();}
-	ajax.setMs(ms);
-	request.getSession().setAttribute("ajaxupload", ajax);
-}
 }
 
